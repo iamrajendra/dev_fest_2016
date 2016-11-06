4ef0d2758963ca6b323bda9fbba1cda356b6522b
iamrajendra <rajendrarajaramv@gmail.com>
2016-11-06 15:24:39 +0530

    picasso

--- app/src/main/java/com/example/rajendra/devfest/MainActivity.java
+++ app/src/main/java/com/example/rajendra/devfest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rajendra.devfest;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,45 +9,120 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView_picasssoImg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        /*
+        * This code represent how image use image from network
+        *
+        *
+        *
+        * */
+        imageView_picasssoImg = (ImageView) findViewById(R.id.img);
+        Picasso.with(this).
+// network url
+        load("https://raw.githubusercontent.com/iamrajendra/dev_fest_2016/master/dev_fest.jpg").
+// error placeholder
+        error(R.drawable.ic_error_red_24dp).
+// normal place holder
+        placeholder(R.drawable.ic_photo_black_24dp).
+// resize image
+//        resize(200, 200).
+//crop the image
+//        centerCrop()
+
+
+        into(imageView_picasssoImg, new Callback() {
+    @Override
+    public void onSuccess() {
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onError() {
+
+    }
+});
+
+
+
+       /*
+        * This code represent how image use image from disk
+        *
+        *
+        *
+        * */
+        imageView_picasssoImg = (ImageView) findViewById(R.id.img);
+        Picasso.with(this).
+// network url
+        load(R.drawable.github_image).
+// error placeholder
+        error(R.drawable.ic_error_red_24dp).
+// normal place holder
+        placeholder(R.drawable.ic_photo_black_24dp).
+// resize image
+//        resize(200, 200).
+//crop the image
+//        centerCrop()
+
+
+        into(imageView_picasssoImg, new Callback() {
+    @Override
+    public void onSuccess() {
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onError() {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
+});
+
+      /*
+        * This code represent how image use image from memory
+        *
+        *
+        *
+        * */
+        imageView_picasssoImg = (ImageView) findViewById(R.id.img);
+        Picasso.with(this).
+// network url
+        load(R.drawable.github_image).
+// error placeholder
+        error(R.drawable.ic_error_red_24dp).
+// normal place holder
+        placeholder(R.drawable.ic_photo_black_24dp).
+// resize image
+//        resize(200, 200).
+//crop the image
+//        centerCrop()
+
+
+        into(imageView_picasssoImg, new Callback() {
+    @Override
+    public void onSuccess() {
+
+    }
+
+    @Override
+    public void onError() {
+
+    }
+});
+
+
+    }
+
+
 }
